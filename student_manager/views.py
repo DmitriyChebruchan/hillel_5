0874b28f84efbcc75c27d5fa9ce251947cf64949
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-09-26 20:14:58 +0300

    revised adding students function

--- student_manager/views.py
+++ student_manager/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
-from faker import Faker
 from django.http import HttpResponse
+from faker import Faker
+from student_manager.models import Student
 
 
 def index_page(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def generate_student(request):
-    return render(request, 'student_generator.html')
+    
+    fake = Faker()
+    new_student = Student(name=fake.name(), address=fake.address())
+    new_student.save()
+
+    return render(request, "student_generator.html")
 
 
 def generate_students(request):
     try:
-        count = int(request.GET.get('count'))
+        count = int(request.GET.get("count"))
         if 0 <= count <= 100:
-            return HttpResponse('success')
+            i=0
+            while i < count:
+                fake = Faker()
+                new_student = Student(name=fake.name(), address=fake.address())
+                new_student.save()
+                i += 1
+            return HttpResponse("success")
         else:
-            return HttpResponse('error')
+            return HttpResponse("error: Quantity of students is out of range: <br>0 - 100")
     except ValueError:
-        return HttpResponse('error')
-
+        return HttpResponse("error: Quantity of students to be countable")
